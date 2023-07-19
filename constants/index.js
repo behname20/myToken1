cf9e9cdf6339622d9705c802d96975d17d116c33
behname20 <97705756+behname20@users.noreply.github.com>
2023-07-19 03:53:12 -0700

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xf1b7501cda1c6a664131f455d8f47d7d6d5d5b34";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
